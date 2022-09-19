074023806cad4efd5612eb57b50586139dde0930
Blake Burns <bburns@mysgi.com>
2022-09-19 00:56:45 -0400

    updated link to js page in html to reflect new path, commented out provided starter code to avoid accidentally screwing it up, added variables and strings to generatePassword function, very inefficiently

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,64 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+function generatePassword() {
 
-  passwordText.value = password;
+  var numCharacters = prompt("How long would you like your password? (Minimum 8 Characters / Maximum 128 Characters)");
+  if (numCharacters > 7 && < 129) {
 
+  } else {
+
+  }
+
+  var upperCase = prompt("Include UPPERCASE letters? (Yes or No)");
+  var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  if (upperCase = "Yes" || "yes" || "YES" || "Y" || "y") {
+
+  } if else (upperCase = "No" || "no" || "NO" || "N" || "n") {
+    
+  } else {
+
+  };
+  
+  var lowerCase = prompt("Include lowercase letters? (Yes or No)");
+  var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  if (lowerCase = "Yes" || "yes" || "YES" || "Y" || "y") {
+
+  } if else (lowerCase = "No" || "no" || "NO" || "N" || "n") {
+    
+  } else {
+    
+  };
+
+
+  var numbers = prompt("Include numbers? (Yes or No)");
+  var numOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  if (numbers = "Yes" || "yes" || "YES" || "Y" || "y") {
+
+  } if else (numbers = "No" || "no" || "NO" || "N" || "n") {
+    
+  } else {
+    
+  };
+
+  var special = prompt("Include special characters? (Yes or No)");
+  var specialCharacters = [“!”, “"”, “#”, “$”, “%”, “&”, “'”, “(”, “)”, “*”, “+”, “,”, “-”, “.”, “/”, “:”, “;”, “<”, “=”, “>”, “?”, “@”, “[”, “\”, “]”, “^”, “_”, “`”, “{”, “|”, “}”, “~”]
+  if (special = "Yes" || "yes" || "YES" || "Y" || "y") {
+
+  } if else (special = "No" || "no" || "NO" || "N" || "n") {
+    
+  } else {
+    
+  };
+
+  return "upperCase + lowerCase + numbers + special";
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// generateBtn.addEventListener("click", writePassword);

75c172332e20a68a68ac5e7874bc36fa1404ca8a
Blake Burns <bburns@mysgi.com>
2022-09-19 23:29:39 -0400

    added generatePassword function

--- Assets/script.js
+++ Assets/script.js
@@ -1,64 +1,60 @@
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
+  var possibleCharacters = []
+
   var numCharacters = prompt("How long would you like your password? (Minimum 8 Characters / Maximum 128 Characters)");
-  if (numCharacters > 7 && < 129) {
-
-  } else {
-
+  if (numCharacters < 8 || numCharacters > 128) {
+    alert("Please enter a number from 8 to 128...");
+    return
+  } else if (isNaN(numCharacters)) {
+    alert("Please enter a number...");
+    return
   }
 
-  var upperCase = prompt("Include UPPERCASE letters? (Yes or No)");
-  var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  if (upperCase = "Yes" || "yes" || "YES" || "Y" || "y") {
+  var upperCaseQuery = confirm("Include Uppercase Letters?     OK = YES / Cancel = NO");
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  if (upperCaseQuery === true) {
+    possibleCharacters.push(upperCase);
+  }
 
-  } if else (upperCase = "No" || "no" || "NO" || "N" || "n") {
-    
-  } else {
+  var lowerCaseQuery = confirm("Include Lowercase Letters?     OK = YES / Cancel = NO");
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  if (lowerCaseQuery === true) {
+    possibleCharacters.push(lowerCase);
+  }
 
-  };
-  
-  var lowerCase = prompt("Include lowercase letters? (Yes or No)");
-  var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  if (lowerCase = "Yes" || "yes" || "YES" || "Y" || "y") {
+  var numberQuery = confirm("Include Numbers?     OK = YES / Cancel = NO");
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  if (numberQuery === true) {
+    possibleCharacters.push(numbers);
+  }
 
-  } if else (lowerCase = "No" || "no" || "NO" || "N" || "n") {
-    
-  } else {
-    
-  };
+  var specialQuery = confirm("Include Special Characters?     OK = YES / Cancel = NO");
+  var specialCharacters = ["!", "“", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+  if (specialQuery === true) {
+    possibleCharacters.push(specialCharacters);
+  }
 
+  if (possibleCharacters.length === 0) {
+    alert("You have chosen NO to Uppercase Letters, Lowercase Letters, Numbers, and Special Characters. We have no available characters to generate your password...");
+    return
+  }
 
-  var numbers = prompt("Include numbers? (Yes or No)");
-  var numOptions = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  if (numbers = "Yes" || "yes" || "YES" || "Y" || "y") {
-
-  } if else (numbers = "No" || "no" || "NO" || "N" || "n") {
-    
-  } else {
-    
-  };
-
-  var special = prompt("Include special characters? (Yes or No)");
-  var specialCharacters = [“!”, “"”, “#”, “$”, “%”, “&”, “'”, “(”, “)”, “*”, “+”, “,”, “-”, “.”, “/”, “:”, “;”, “<”, “=”, “>”, “?”, “@”, “[”, “\”, “]”, “^”, “_”, “`”, “{”, “|”, “}”, “~”]
-  if (special = "Yes" || "yes" || "YES" || "Y" || "y") {
-
-  } if else (special = "No" || "no" || "NO" || "N" || "n") {
-    
-  } else {
-    
-  };
-
-  return "upperCase + lowerCase + numbers + special";
+  var shinyNewPassword = ""
+  for (var i = 0; i < numCharacters; i++) {
+    shinyNewPassword += possibleCharacters[Math.floor(Math.random()*possibleCharacters.length)];
+  }
+  // return shinyNewPassword
+  console.log(shinyNewPassword);
 }
 
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
+  passwordText.value = password;
+}
 
-// }
-
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # code-refactor-password-generator
+
+## Description
 Week 3 Challenge: JavaScript
+Code refactor to add interactive functionality to a random password generator.
+
+## Installation
+
+N/A
+
+## Usage
+
+This random password generator will actually function and provide secure passwords to users in order to keep sensitive data safe from online jerks.
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to the LICENSE in the repo.

5bf7e33b87f64436bb84b5281926477a1428f571
Blake Burns <bburns@mysgi.com>
2023-01-17 13:35:57 -0500

    formatted js

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,49 @@
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 function generatePassword() {
   var possibleCharacters = [];
 
   var numCharacters = prompt(
-    "How long would you like your password? (Minimum 8 Characters / Maximum 128 Characters)"
+    'How long would you like your password? (Minimum 8 Characters / Maximum 128 Characters)'
   );
   if (numCharacters < 8 || numCharacters > 128) {
-    alert("Please enter a number from 8 to 128...");
+    alert('Please enter a number from 8 to 128...');
     return;
   } else if (isNaN(numCharacters)) {
-    alert("Please enter a number...");
+    alert('Please enter a number...');
     return;
   }
 
   var upperCaseQuery = confirm(
-    "Include Uppercase Letters?     OK = YES / Cancel = NO"
+    'Include Uppercase Letters?     OK = YES / Cancel = NO'
   );
   var upperCase = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
   ];
   if (upperCaseQuery === true) {
     possibleCharacters = possibleCharacters.concat(upperCase);
@@ -51,81 +51,81 @@ function generatePassword() {
   console.log(upperCaseQuery);
 
   var lowerCaseQuery = confirm(
-    "Include Lowercase Letters?     OK = YES / Cancel = NO"
+    'Include Lowercase Letters?     OK = YES / Cancel = NO'
   );
   var lowerCase = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
   ];
   if (lowerCaseQuery === true) {
     possibleCharacters = possibleCharacters.concat(lowerCase);
   }
 
-  var numberQuery = confirm("Include Numbers?     OK = YES / Cancel = NO");
-  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var numberQuery = confirm('Include Numbers?     OK = YES / Cancel = NO');
+  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   if (numberQuery === true) {
     possibleCharacters = possibleCharacters.concat(numbers);
   }
 
   var specialQuery = confirm(
-    "Include Special Characters?     OK = YES / Cancel = NO"
+    'Include Special Characters?     OK = YES / Cancel = NO'
   );
   var specialCharacters = [
-    "!",
-    "“",
-    "#",
-    "$",
-    "%",
-    "&",
+    '!',
+    '“',
+    '#',
+    '$',
+    '%',
+    '&',
     "'",
-    "(",
-    ")",
-    "*",
-    "+",
-    ",",
-    "-",
-    ".",
-    "/",
-    ":",
-    ";",
-    "<",
-    "=",
-    ">",
-    "?",
-    "@",
-    "[",
-    "]",
-    "^",
-    "_",
-    "`",
-    "{",
-    "|",
-    "}",
-    "~",
+    '(',
+    ')',
+    '*',
+    '+',
+    ',',
+    '-',
+    '.',
+    '/',
+    ':',
+    ';',
+    '<',
+    '=',
+    '>',
+    '?',
+    '@',
+    '[',
+    ']',
+    '^',
+    '_',
+    '`',
+    '{',
+    '|',
+    '}',
+    '~',
   ];
   if (specialQuery === true) {
     possibleCharacters = possibleCharacters.concat(specialCharacters);
@@ -133,12 +133,12 @@ function generatePassword() {
 
   if (possibleCharacters.length === 0) {
     alert(
-      "You have chosen NO to Uppercase Letters, Lowercase Letters, Numbers, and Special Characters. We have no available characters to generate your password..."
+      'You have chosen NO to Uppercase Letters, Lowercase Letters, Numbers, and Special Characters. We have no available characters to generate your password...'
     );
     return;
   }
 
-  var shinyNewPassword = "";
+  var shinyNewPassword = '';
   for (var i = 0; i < numCharacters; i++) {
     shinyNewPassword +=
       possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
@@ -148,9 +148,9 @@ function generatePassword() {
 
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 }
 
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);

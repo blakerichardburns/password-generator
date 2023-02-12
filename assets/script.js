6d9aa7427b22f9839260449dd518366d9407018d
Blake Burns <bburns@mysgi.com>
2023-02-12 17:57:39 -0500

    major refactor: broke out parts of generatePassword function into smaller functions, character option lists originate as strings rather an arrays, other miscellaneous reorganziation and cleanup

--- assets/script.js
+++ assets/script.js
@@ -1,156 +1,114 @@
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
+var passwordParameters = {
+  numberOfCharacters: 10,
+  lowercaseLetters: true,
+  uppercaseLetters: true,
+  numbers: true,
+  specialCharacters: true,
+};
 
 function generatePassword() {
-  var possibleCharacters = [];
+  promptsForUser();
 
-  var numCharacters = prompt(
-    'How long would you like your password? (Minimum 8 Characters / Maximum 128 Characters)'
-  );
-  if (numCharacters < 8 || numCharacters > 128) {
-    alert('Please enter a number from 8 to 128...');
-    return;
-  } else if (isNaN(numCharacters)) {
-    alert('Please enter a number...');
-    return;
-  }
-
-  var upperCaseQuery = confirm(
-    'Include Uppercase Letters?     OK = YES / Cancel = NO'
-  );
-  var upperCase = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z',
-  ];
-  if (upperCaseQuery === true) {
-    possibleCharacters = possibleCharacters.concat(upperCase);
-  }
-  console.log(upperCaseQuery);
-
-  var lowerCaseQuery = confirm(
-    'Include Lowercase Letters?     OK = YES / Cancel = NO'
-  );
-  var lowerCase = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  ];
-  if (lowerCaseQuery === true) {
-    possibleCharacters = possibleCharacters.concat(lowerCase);
-  }
-
-  var numberQuery = confirm('Include Numbers?     OK = YES / Cancel = NO');
-  var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-  if (numberQuery === true) {
-    possibleCharacters = possibleCharacters.concat(numbers);
-  }
-
-  var specialQuery = confirm(
-    'Include Special Characters?     OK = YES / Cancel = NO'
-  );
-  var specialCharacters = [
-    '!',
-    '“',
-    '#',
-    '$',
-    '%',
-    '&',
-    "'",
-    '(',
-    ')',
-    '*',
-    '+',
-    ',',
-    '-',
-    '.',
-    '/',
-    ':',
-    ';',
-    '<',
-    '=',
-    '>',
-    '?',
-    '@',
-    '[',
-    ']',
-    '^',
-    '_',
-    '`',
-    '{',
-    '|',
-    '}',
-    '~',
-  ];
-  if (specialQuery === true) {
-    possibleCharacters = possibleCharacters.concat(specialCharacters);
-  }
-
-  if (possibleCharacters.length === 0) {
+  while (!validatePasswordParameters()) {
     alert(
-      'You have chosen NO to Uppercase Letters, Lowercase Letters, Numbers, and Special Characters. We have no available characters to generate your password...'
+      "You have chosen NO to Uppercase Letters, Lowercase Letters, Numbers, and Special Characters. There are no available characters to generate your password..."
     );
-    return;
+    promptsForUser();
   }
 
-  var shinyNewPassword = '';
-  for (var i = 0; i < numCharacters; i++) {
-    shinyNewPassword +=
-      possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
+  var shinyNewPassword = "";
+  var availableCharacterOptions = generateAvailableCharacterOptions();
+  for (var i = 0; i < passwordParameters.numberOfCharacters; i++) {
+    shinyNewPassword += randomCharacterGenerator(availableCharacterOptions);
   }
+
   return shinyNewPassword;
+}
+
+function selectNumberOfCharacters() {
+  var numberOfCharacters = prompt(
+    "How long would you like your password? (Minimum 8 Characters / Maximum 128 Characters)"
+  );
+
+  if (
+    numberOfCharacters === null ||
+    isNaN(numberOfCharacters) ||
+    numberOfCharacters < 8 ||
+    numberOfCharacters > 128
+  ) {
+    alert("Please enter a number from 8 to 128...");
+    selectNumberOfCharacters();
+  }
+
+  return numberOfCharacters;
+}
+
+function promptsForUser() {
+  passwordParameters.numberOfCharacters = selectNumberOfCharacters();
+  passwordParameters.uppercaseLetters = confirm(
+    "Include Uppercase Letters?     OK = YES / Cancel = NO"
+  );
+  passwordParameters.lowercaseLetters = confirm(
+    "Include Lowercase Letters?     OK = YES / Cancel = NO"
+  );
+  passwordParameters.numbers = confirm(
+    "Include Numbers?     OK = YES / Cancel = NO"
+  );
+  passwordParameters.specialCharacters = confirm(
+    "Include Special Characters?     OK = YES / Cancel = NO"
+  );
+}
+
+function validatePasswordParameters() {
+  return (
+    passwordParameters.lowercaseLetters ||
+    passwordParameters.uppercaseLetters ||
+    passwordParameters.numbers ||
+    passwordParameters.specialCharacters
+  );
+}
+
+function generateAvailableCharacterOptions() {
+  var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+  var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  var numbers = "0123456789".split("");
+  var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".split("");
+
+  var availableCharacterOptionsArray = [];
+
+  if (passwordParameters.lowercaseLetters) {
+    availableCharacterOptionsArray =
+      availableCharacterOptionsArray.concat(lowercaseLetters);
+  }
+
+  if (passwordParameters.uppercaseLetters) {
+    availableCharacterOptionsArray =
+      availableCharacterOptionsArray.concat(uppercaseLetters);
+  }
+
+  if (passwordParameters.numbers) {
+    availableCharacterOptionsArray =
+      availableCharacterOptionsArray.concat(numbers);
+  }
+
+  if (passwordParameters.specialCharacters) {
+    availableCharacterOptionsArray =
+      availableCharacterOptionsArray.concat(specialCharacters);
+  }
+
+  return availableCharacterOptionsArray;
+}
+
+function randomCharacterGenerator(characterArray) {
+  return characterArray[Math.floor(Math.random() * characterArray.length)];
 }
 
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener("click", writePassword);
